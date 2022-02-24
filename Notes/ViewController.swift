5874ebd6123d170197b7d7c5e5dae7236c03d931
Elina Kirtaeva <elin-ru1@yandex.ru>
2022-02-24 14:17:50 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Элина Рупова on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+
+    }
+    
+    
+}
